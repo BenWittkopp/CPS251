2bc0f9ed9ea37fb8edf32c5d0003201fd99899ca
BenWittkopp <bwittkopp@wccnet.edu>
2022-10-02 19:18:03 -0400

    updates

---
+++ Assignments/Assignment_1/Assignment_1/app/src/main/java/com/example/assignment_1/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.assignment_1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+    }
+
+
+
+
